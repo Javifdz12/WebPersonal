c5199e40c3fda4165a94aeb84c3a4a27d9902451
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-11-20 18:28:41 +0100

    gg

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home, about
+from core.views import home,about,contact
 urlpatterns = [
-    path('',home, name="home"),
+    path('', home, name="home"),
     path('about/',about, name="about"),
+    path('contact/',contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
