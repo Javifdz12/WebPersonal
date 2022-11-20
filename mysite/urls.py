e0203934437ec99d9347e324c9e1215ce248f7ee
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-11-20 18:54:42 +0100

    gg

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from core import views
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about/',views.about, name="about"),
+    #path('about/',views.about, name="about"),
     path('contact/',views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
